1e19d89b3b190bb8655f95c5bcd3846e545e819f
Daniil Romankin <d.romankin@g.nsu.ru>
2024-09-04 20:35:09 +0700

    sample

--- Task_1_1_1/src/main/java/ru/nsu/romankin/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/romankin/Main.java
@@ -1,8 +1,8 @@
 package ru.nsu.romankin;
 
 public class Main {
-    public static int sum(int a,int b){
-        return a+b;
+    public static int sum(int a, int b) {
+        return a + b;
     }
     public static void main(String[] args) {
         System.out.println("Hello world!");
--- Task_1_1_1/src/test/java/ru/nsu/romankin/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/romankin/MainTest.java
@@ -6,7 +6,7 @@ import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
 @Test
-    void SampleTest(){
-    assertEquals(3,Main.sum(1,2));
+    void SampleTest() {
+    assertEquals(3, Main.sum(1, 2));
     }
 }
