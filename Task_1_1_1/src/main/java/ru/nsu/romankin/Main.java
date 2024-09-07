eeb134071906143d14e9ae0c1481cdb7bffefe8b
Daniil Romankin <d.romankin@g.nsu.ru>
2024-09-08 00:13:39 +0700

    1st commit

--- Task_1_1_1/src/main/java/ru/nsu/romankin/Main.java
+++ Task_1_1_1/src/main/java/ru/nsu/romankin/Main.java
@@ -1,10 +1,41 @@
 package ru.nsu.romankin;
 
 public class Main {
-    public static int sum(int a, int b) {
-        return a + b;
+    public static void build_heap(int[] arr, int i,int n) { /*arr - array, i - index, n - amount of elements*/
+        int index_of_max = i;
+        if ((2 * i + 1) < n && arr[2 * i + 1] > arr[index_of_max]) {
+            index_of_max=2*i+1; /*left son*/
+        }
+        if ((2 * i + 2) < n && arr[2 * i + 2] > arr[index_of_max] ) {
+            index_of_max = 2 * i + 2; /*right son*/
+        }
+        if (i != index_of_max) {
+            int temp = arr[index_of_max];
+            arr[index_of_max] = arr[i];
+            arr[i] = temp;
+            build_heap(arr, index_of_max, n);
+        }
+    }
+    public static int[] heapsort(int[] arr){
+        int n=arr.length;
+        for (int j = n / 2; j >= 0; j--) {
+            build_heap(arr, j, n);
+        }
+        for (int i = n - 1; i >= 0; i--){
+
+            int t = arr[0];
+            arr[0] = arr[i];
+            arr[i] =t;
+            build_heap(arr, 0, i);
+        }
+        return arr;
     }
     public static void main(String[] args) {
-        System.out.println("Hello world!");
+        int[] arr=new int[] {1,1,1,87,4,0,8,5,7,7};
+        heapsort(arr);
+        for (int i=0;i<arr.length;i++){
+            System.out.printf("%d, ",arr[i]);
+        }
+
     }
 }
--- Task_1_1_1/src/test/java/ru/nsu/romankin/MainTest.java
+++ Task_1_1_1/src/test/java/ru/nsu/romankin/MainTest.java
@@ -2,11 +2,33 @@ package ru.nsu.romankin;
 
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class MainTest {
-@Test
-    void SampleTest() {
-    assertEquals(3, Main.sum(1, 2));
+    @Test
+    void Test_1_positive_result() {
+    assertArrayEquals(new int[] {1,2,3,4,5},Main.heapsort(new int[]{3,1,2,5,4}));
+    }
+    @Test
+    void Test_2_negative_result (){
+        assertFalse(Arrays.equals(new int[]{1,3,2,4,5}, Main.heapsort(new int[]{3, 1, 2, 5, 4})));
+    }
+    @Test
+    void Test_3_negative_numbers(){
+        assertArrayEquals(new int[] {-8,-5,0,1,12},Main.heapsort(new int[]{-5,-8,12,1,0}));
+    }
+    @Test
+    void Test_4_negative_numbers_negative_result(){
+        assertFalse(Arrays.equals(new int[] {12,-8,-5,1,0},Main.heapsort(new int[]{-5,-8,12,1,0})));
+    }
+    @Test
+    void Test_5_repeating_numbers(){
+        assertArrayEquals(new int[] {0, 1, 1, 1, 4, 5, 7, 7, 8, 87},Main.heapsort(new int[] {1,1,1,87,4,0,8,5,7,7}));
+    }
+    @Test
+    void Test_6_one_element(){
+        assertArrayEquals(new int[] {-8},Main.heapsort(new int[] {-8}));
     }
 }
