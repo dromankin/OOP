dd707c35f28509d8c1a89ab7ae2263f5e1afe52e
dromankin <dromankin@users.noreply.github.com>
2024-09-09 09:51:40 +0000

    Deploying to gh-pages from @ dromankin/OOP@59fda7f97bf22e6e7ebc44163ea0fc3ba5c4f8cd 🚀

--- Task_1_1_1/type-search-index.js
+++ Task_1_1_1/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.romankin","l":"Main"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.romankin","l":"HeapSort"}]
