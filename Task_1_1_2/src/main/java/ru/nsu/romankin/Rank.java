a0187e233f5f1163cf20dfdab3864b146a7c8577
Daniil Romankin <d.romankin@g.nsu.ru>
2024-09-20 14:22:06 +0700

    added rank

---
+++ Task_1_1_2/src/main/java/ru/nsu/romankin/Rank.java
@@ -0,0 +1,24 @@
+package ru.nsu.romankin;
+
+public enum Rank {
+    TW0(2,"Двойка"),
+    THREE(3,"Тройка"),
+    FOUR(4,"Четвёрка"),
+    FIVE(5,"Пятёрка"),
+    SIX(6,"Шестёрка"),
+    SEVEN(7,"Семёрка"),
+    EIGHT(8,"Восьмёрка"),
+    NINE(9,"Девятка"),
+    TEN(10,"Десятка"),
+    JACK(10,"Валет"),
+    QUEEN(10,"Дама"),
+    KING(10,"Король"),
+    ACE(11,"Туз"),
+    ;
+    Rank(int value, String rankname){
+        this.value = value;
+        this.rankname = rankname;
+    }
+    int value;
+    String rankname;
+}
