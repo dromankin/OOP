1afe2525ae314fa6c32d5f92b71a9d2b4e63ec2c
dromankin <dromankin@users.noreply.github.com>
2024-09-20 07:22:51 +0000

    Deploying to gh-pages from @ dromankin/OOP@486cc263c9e7aa6d742219f2e11ad5b8192536cf 🚀

--- Task_1_1_2/type-search-index.js
+++ Task_1_1_2/type-search-index.js
@@ -1,1 +1,1 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.romankin","l":"Card"},{"p":"ru.nsu.romankin","l":"Deck"},{"p":"ru.nsu.romankin","l":"Main"},{"p":"ru.nsu.romankin","l":"Player"}]
+typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"ru.nsu.romankin","l":"Card"},{"p":"ru.nsu.romankin","l":"Deck"},{"p":"ru.nsu.romankin","l":"Main"},{"p":"ru.nsu.romankin","l":"Player"},{"p":"ru.nsu.romankin","l":"Rank"}]
