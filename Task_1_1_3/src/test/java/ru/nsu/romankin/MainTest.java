6b89ed2e44a8dc8118001062906e6cdf93f70c80
Daniil Romankin <d.romankin@g.nsu.ru>
2024-09-28 21:03:15 +0700

    added test

---
+++ Task_1_1_3/src/test/java/ru/nsu/romankin/MainTest.java
@@ -0,0 +1,14 @@
+package ru.nsu.romankin;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class MainTest {
+
+    @Test
+    void test() {
+        Main.main(null);
+        assertTrue(true);
+    }
+}
