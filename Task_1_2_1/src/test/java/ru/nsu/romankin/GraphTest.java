1a03377304dec2af08438f5b1682ad9ef0be7032
Daniil Romankin <d.romankin@g.nsu.ru>
2024-10-20 00:05:45 +0700

    Add rewiewdog fixes.

--- Task_1_2_1/src/test/java/ru/nsu/romankin/GraphTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/romankin/GraphTest.java
@@ -5,7 +5,6 @@ import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
 import static org.junit.jupiter.api.Assertions.assertTrue;
 
-
 import java.io.FileNotFoundException;
 import java.util.List;
 import java.util.stream.Stream;
