86a5292aab7ca12d87f25d079937e4e4aced1174
Daniil Romankin <d.romankin@g.nsu.ru>
2024-10-20 00:04:45 +0700

    Add rewiewdog fixes.

--- Task_1_2_1/src/test/java/ru/nsu/romankin/GraphTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/romankin/GraphTest.java
@@ -1,9 +1,10 @@
 package ru.nsu.romankin;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
-import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertFalse;
 import static org.junit.jupiter.api.Assertions.assertNotEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+
 
 import java.io.FileNotFoundException;
 import java.util.List;
--- Task_1_2_1/src/test/java/ru/nsu/romankin/TopologicalSortTest.java
+++ Task_1_2_1/src/test/java/ru/nsu/romankin/TopologicalSortTest.java
@@ -1,7 +1,7 @@
 package ru.nsu.romankin;
 
-import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
 
 import java.util.List;
 import java.util.stream.Stream;
