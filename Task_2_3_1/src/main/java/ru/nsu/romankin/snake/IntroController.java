31eeea4298136079bab7c0cfa56e3e44a3e0eb7c
Daniil Romankin <d.romankin@g.nsu.ru>
2025-04-02 01:10:51 +0700

    Reviewdog

--- Task_2_3_1/src/main/java/ru/nsu/romankin/snake/IntroController.java
+++ Task_2_3_1/src/main/java/ru/nsu/romankin/snake/IntroController.java
@@ -47,11 +47,11 @@ public class IntroController {
         URL fxmlUrl = getClass().getResource("/game.fxml");
         FXMLLoader loader = new FXMLLoader(fxmlUrl);
         Parent root = loader.load();
-        Scene scene = new Scene(root);
         Controller controller = loader.getController();
         controller.setDifficulty(speed);
         stage = new Stage();
         stage.setTitle("Snake");
+        Scene scene = new Scene(root);
         stage.setScene(scene);
         stage.setResizable(false);
         stage.show();
